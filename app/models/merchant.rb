e10bf80d8419b26de14ed5add5de17cf7a80bd3c
Kami Boers <kamiboers@gmail.com>
2016-05-31 00:36:53 -0600

    Remaining Merchant Business Intelligence (#2)
    * create functional ActiveRecord methods to return most items and merchant revenue by date
    * tests for the above methods
    * revenue by date passing with mm/dd/yyyy date format - this may need to change in the future
    * 100% test coverage

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -19,8 +19,27 @@ class Merchant < ActiveRecord::Base
     return where("lower(name) = ?", params[:name].downcase).as_json if params[:name]
   end
 
+  def self.top_by_revenue(n)
+    all.sort_by(&:sales).reverse.first(n)
+  end
+
+  def self.top_by_item_count(n)
+    all.sort_by(&:item_count).reverse.first(n)
+  end
+
   def sales
-    invoices.paid.joins(:invoice_items).sum("quantity * unit_price")
+    invoices.paid.includes(:invoice_items).sum("quantity * unit_price")
+  end
+
+  def item_count
+    items.count
+  end
+
+  def revenue_by_date(date_string)
+    format_string = "%m/%d/" + (date_string =~ /\d{4}/ ? "%Y" : "%y")
+    date = Date.parse(date_str) rescue Date.strptime(date_string, format_string)
+    
+    invoices.where(updated_at: date.beginning_of_day..date.end_of_day).joins(:invoice_items).paid.pluck("quantity * unit_price").sum
   end
 
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -28,4 +28,64 @@ RSpec.describe Merchant, type: :model do
     expect(merchant.sales).to eq(0)
   end
 
+  it "returns top merchants by sales revenue" do
+    create_merchant(2)
+    fourth_ranked = Merchant.first
+    third_ranked = Merchant.last 
+    create_merchant
+    second_ranked = Merchant.last
+    create_merchant
+    first_ranked = Merchant.last
+
+    allow(fourth_ranked).to receive(:sales).and_return(05)
+    allow(third_ranked).to receive(:sales).and_return(10)
+    allow(second_ranked).to receive(:sales).and_return(15)
+    allow(first_ranked).to receive(:sales).and_return(20)
+    top_three = Merchant.top_by_revenue(3)
+
+    expect(top_three).to include(first_ranked)
+    expect(top_three).to include(second_ranked)
+    expect(top_three).to include(third_ranked)
+    expect(top_three).not_to include(fourth_ranked)
+  end
+
+  it "returns top merchants by number of items" do
+    create_merchant(2)
+    fourth_ranked = Merchant.first
+    third_ranked = Merchant.last 
+    create_merchant
+    second_ranked = Merchant.last
+    create_merchant
+    first_ranked = Merchant.last
+
+    allow(fourth_ranked).to receive(:item_count).and_return(05)
+    allow(third_ranked).to receive(:item_count).and_return(10)
+    allow(second_ranked).to receive(:item_count).and_return(15)
+    allow(first_ranked).to receive(:item_count).and_return(20)
+    top_three = Merchant.top_by_item_count(3)
+
+    expect(top_three).to include(first_ranked)
+    expect(top_three).to include(second_ranked)
+    expect(top_three).to include(third_ranked)
+    expect(top_three).not_to include(fourth_ranked)
+  end
+
+  it "returns revenue by date of transaction" do
+    create_merchant
+    merchant = Merchant.last
+    create_invoice(1, "shipped", 1, merchant.id)
+    invoice = Invoice.last
+    create_invoice_item(1, 20, 20, 1, invoice.id)
+    create_transaction(1, "cc_number", "success", invoice.id)
+
+    today = (Date.today).strftime("%m/%d/%Y")
+    yesterday = (Date.yesterday).strftime("%m/%d/%Y")
+
+    revenue_today = merchant.revenue_by_date(today)
+    revenue_yesterday = merchant.revenue_by_date(yesterday)
+
+    expect(revenue_today).to eq(400)
+    expect(revenue_yesterday).to eq(0)
+  end
+
 end
